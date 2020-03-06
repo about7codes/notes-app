deefb34da4314272df59a1087f59b5d0cc05d696
about7code <sanaullahhaifz@gmail.com>
2020-03-06 12:05:06 +0400

    Updates

---
+++ precache-manifest.c7f09a17d4df3c9f8d2f878864b482b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e08ca3827acd7796667826ac0e475a2",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "265db95704a8358210ff",
+    "url": "/notes-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9f2e9f06bbb4f34fb281",
+    "url": "/notes-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "265db95704a8358210ff",
+    "url": "/notes-app/static/js/2.cce3f81f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/notes-app/static/js/2.cce3f81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2e9f06bbb4f34fb281",
+    "url": "/notes-app/static/js/main.80989e09.chunk.js"
+  },
+  {
+    "revision": "87be85906e8c64bae6fb",
+    "url": "/notes-app/static/js/runtime-main.2e449a58.js"
+  }
+]);
